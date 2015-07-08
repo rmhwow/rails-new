e4b64430f36c90529a49e6e3cbf66dbec86a780f
Morgan OLeary <rmhwow@gmail.com>
2015-07-07 22:20:21 -0400

    add seed data

--- NewApp/db/seeds.rb
+++ NewApp/db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+ 
+ # Create Posts
+ 50.times do
+   Post.create!(
+     title:  Faker::Lorem.sentence,
+     body:   Faker::Lorem.paragraph
+   )
+ end
+ 
+ my_post = Post.create(
+  title: "Awesome Post",
+  body: "Hey this is my post about the chicken farm"
+  )
+
+ posts = Post.all
+ 
+ # Create Comments
+ 100.times do
+   Comment.create!(
+     post: posts.sample,
+     body: Faker::Lorem.paragraph
+   )
+ end
+
+my_comment = Comment.create(
+  post: my_post,
+  body: "this is awesome"
+  )
+ 
+ puts "Seed finished"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
