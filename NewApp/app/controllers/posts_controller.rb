503d9336ce6e347924af2d97a5c174321feb5cc7
Morgan OLeary <rmhwow@gmail.com>
2015-07-10 16:31:35 -0400

    Add users to the view, db, and models

--- NewApp/app/controllers/posts_controller.rb
+++ NewApp/app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
   end
   def create
     @post = Post.new(params.require(:post).permit(:title, :body))
-    # raise
+    @post.user = current_user
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
--- NewApp/db/seeds.rb
+++ NewApp/db/seeds.rb
@@ -6,66 +6,94 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'faker'
+  # Create Users
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
  
+ users = User.all
+ 
+ # Note: by calling `User.new` instead of `create`,
+ # we create an instance of User which isn't immediately saved to the database.
+ 
+ # The `skip_confirmation!` method sets the `confirmed_at` attribute
+ # to avoid triggering an confirmation email when the User is saved.
+ 
+ # The `save` method then saves this User to the database.
  # Create Posts
  50.times do
    Post.create!(
+    user: users.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
  end
 
-my_post = Post.where(
-  title: "Awesome Post",
-  body: "Hey this is my post about the chicken farm"
-  ).first 
+# my_post = Post.where(
+#   title: "Awesome Post",
+#   body: "Hey this is my post about the chicken farm"
+#   ).first 
 
- unless my_post
- my_post = Post.create(
-  title: "Awesome Post",
-  body: "Hey this is my post about the chicken farm"
-  )
- end
+#  unless my_post
+#  my_post = Post.create(
+#   title: "Awesome Post",
+#   body: "Hey this is my post about the chicken farm"
+#   )
+#  end
 
  posts = Post.all
  
  # Create Comments
  100.times do
    Comment.create!(
+    # user: users.sample,
      post: posts.sample,
      body: Faker::Lorem.paragraph
    )
  end
 
-my_comment = Comment.where(
-  post: my_post,
-  body: "this is awesome"
-  ).first
+# my_comment = Comment.where(
+#   post: my_post,
+#   body: "this is awesome"
+#   ).first
 
- unless my_comment
-  Comment.create(
-  post: my_post,
-  body: "this is awesome"
-  )
-end
-15.times do
-   Advertisement.create!(
-     title:  Faker::Lorem.words(4).join(" "),
-     copy:   Faker::Lorem.paragraph,
-     price:  Faker::Number.positive
-   )
- end
+#  unless my_comment
+#   Comment.create(
+#   post: my_post,
+#   body: "this is awesome"
+#   )
+# end
+# 15.times do
+#    Advertisement.create!(
+#      title:  Faker::Lorem.words(4).join(" "),
+#      copy:   Faker::Lorem.paragraph,
+#      price:  Faker::Number.positive
+#    )
+#  end
 
- 10.times do 
-  Question.create!(
-    title: Faker::Lorem.words(4).join(" "),
-    body: Faker::Lorem.paragraph,
-    resolved: false
-    )
-end
+#  10.times do 
+#   Question.create!(
+#     title: Faker::Lorem.words(4).join(" "),
+#     body: Faker::Lorem.paragraph,
+#     resolved: false
+#     )
+# end
+
+
+ user = User.first
+ user.skip_reconfirmation!
+ user.update_attributes!(
+   email: 'youremail@email.com',
+   password: 'helloworld'
+ )
 
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
- puts "#{Advertisement.count} advertisements created"
-  puts "#{Question.count} questions created"
