6af77dc511aa228ef8e5f7fd47a8020ff70941e9
Morgan OLeary <rmhwow@gmail.com>
2015-07-10 19:25:33 -0400

    moving the app to new folder

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+  has_many :comments
+  belongs_to :user
+
+  # default_scope {order('created_at DESC')}
+  scope :ordered_by_title, -> {order(title: :asc)}
+  scope :ordered_by_reverse_created_at, -> {order(created_at: :desc) } 
+end
