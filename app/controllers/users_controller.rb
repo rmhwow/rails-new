94c074c32eee1d9c19aa49572055400d519b294d
Morgan OLeary <rmhwow@gmail.com>
2015-07-21 15:36:53 -0400

    up votes and down votes work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,21 @@ class UsersController < ApplicationController
      end
    end
  
+  def down_vote_link_classes(post)
+    base = "glyphicon glyphicon-chevron-down "
+    vote = current_user.voted(post)
+    base += "voted" if vote && vote.down_vote?
+    base
+  end
+
+  def up_vote_link_classes(post)
+    base = "glyphicon glyphicon-chevron-up "
+    vote = current_user.voted(post)
+    base += "voted" if vote && vote.up_vote?
+    base
+  end
+
+
    private
  
    def user_params
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,18 @@ module ApplicationHelper
        content_tag :div, capture(&block), class: 'form-group'
      end
    end
+     def down_vote_link_classes(post)
+    base = "glyphicon glyphicon-chevron-down "
+    vote = current_user.voted(post)
+    base += "voted" if vote && vote.down_vote?
+    base
+  end
 
+  def up_vote_link_classes(post)
+    base = "glyphicon glyphicon-chevron-up "
+    vote = current_user.voted(post)
+    base += "voted" if vote && vote.up_vote?
+    base
+  end
   
 end
