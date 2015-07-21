2bc4e2ccefe7022ac7ebc2e700591a8227283bc0
Morgan OLeary <rmhwow@gmail.com>
2015-07-21 17:13:50 -0400

    user can view their own profile

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
  describe "Visiting profiles" do
-
+   include Warden::Test::Helpers
    include TestFactories
 
    before do
@@ -22,6 +22,17 @@ require 'rails_helper'
        expect( page ).to have_content(@post.title)
        expect( page ).to have_content(@comment.body)
      end
+        before do 
+        login_as(@user, :scope => :user)
+      end
+     it "shows the users own profile" do 
+      
+      visit user_path(@user)
+       expect(current_path).to eq(user_path(@user))
 
+       expect( page ).to have_content(@user.name)
+       expect( page ).to have_content(@post.title)
+       expect( page ).to have_content(@comment.body)
+     end
    end
  end
